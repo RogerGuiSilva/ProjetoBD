5bb88e4866e2b71cb5aa04946e37f1f7862bcd52
Roger <rogerguilhermedasilva31@gmail.com>
2025-10-21 19:53:47 -0300

    atualizando

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from tarefa import buscar_tarefas, buscar_tarefa
 
 app = Flask(__name__)
 
@@ -12,9 +13,20 @@ def home():
 # Rota da API
 @app.route("/api", methods=['GET'])
 def index():
+
     return {
         'message': 'API rodando'
     }
+    
+@app.route('/api/tarefas', methods=['GET'])
+def get_tarefas():
+    tarefas = buscar_tarefas()
+    return tarefas
+
+@app.route('/api/tarefa', methods=['GET'])
+def get_tarefa():
+    tarefa = buscar_tarefa()
+    return tarefa
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tarefa.py
@@ -0,0 +1,28 @@
+from flask import jsonify
+
+def buscar_tarefas():
+    tarefas = [
+        {
+            'id': 1,
+            'nome': 'Aprender digitação',
+            'descricao': 'Vamos aumentar o zoom para não errar a digitação',
+            'status': 'Pendente'
+        },
+        {
+            'id': 2,
+            'nome': 'Aprender Python',
+            'descricao': 'Aprender python para programar apis',
+            'status': 'Pendente'
+        }
+    ]
+
+    return jsonify(tarefas)
+
+def buscar_tarefa():
+    tarefa = {
+        'id': 1,
+        'nome': 'Aprender digitação',
+        'descricao': 'Vamos aumentar o zoom para não errar a digitação',
+        'status': 'Pendente'
+    }
+    return jsonify(tarefa)
